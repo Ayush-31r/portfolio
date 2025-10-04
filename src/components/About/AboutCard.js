e9b6c92cc1176a8b536d9aa16515e944b23050cf
Ayush-31r <rai4ayush@gmail.com>
2025-10-04 14:39:03 +0530

    About Section

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,7 +9,7 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Ayush Rai </span>
-            from <span className="purple"> Imphal, India.</span>
+            from <span className="purple"> Lucknow, India.</span>
             <br />
             I am a student at IIIT Manipur, pursuing a B.Tech in Electronics and Communication Engineering.
             <br />
